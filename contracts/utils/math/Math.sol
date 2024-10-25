8a3bfbe8dde6031d33297458bceced9cd139aecb
Justin Silver <jsilver@doublesharp.com>
2024-10-25 11:25:53 -0700

    memory-safe

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -148,7 +148,7 @@ library Math {
             // variables such that product = prod1 * 2²⁵⁶ + prod0.
             uint256 prod0 = x * y; // Least significant 256 bits of the product
             uint256 prod1; // Most significant 256 bits of the product
-            assembly {
+            assembly ("memory-safe") {
                 let mm := mulmod(x, y, not(0))
                 prod1 := sub(sub(mm, prod0), lt(mm, prod0))
             }
@@ -172,7 +172,7 @@ library Math {
 
             // Make division exact by subtracting the remainder from [prod1 prod0].
             uint256 remainder;
-            assembly {
+            assembly ("memory-safe") {
                 // Compute remainder using mulmod.
                 remainder := mulmod(x, y, denominator)
 
@@ -185,7 +185,7 @@ library Math {
             // Always >= 1. See https://cs.stackexchange.com/q/138556/92363.
 
             uint256 twos = denominator & (0 - denominator);
-            assembly {
+            assembly ("memory-safe") {
                 // Divide denominator by twos.
                 denominator := div(denominator, twos)
 
